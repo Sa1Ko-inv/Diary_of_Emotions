0063f605009ddf4782150dc40cab3cb2e63ff574
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-07-30 17:34:50 +0300

    added seed for triggers

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { seedEmotion } from './seeders/emotions';
+import { seedTrigger } from './seeders/trigger';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await seedEmotion(prisma);
+  await seedTrigger(prisma);
 }
 
 main()
