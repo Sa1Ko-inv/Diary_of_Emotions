d555a2ae255878df5ea47e50818fd150f8b4d737
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-08-02 15:03:12 +0300

    added hash password 'User' and fix method update for user

--- api/src/user/dto/update-user.dto.ts
+++ api/src/user/dto/update-user.dto.ts
@@ -2,3 +2,36 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
+
+//
+// import { IsDate, IsEmail, IsOptional, IsString } from 'class-validator';
+// import { Transform } from 'class-transformer';
+//
+// export class UpdateUserDto {
+//   @IsOptional()
+//   @IsEmail()
+//   email?: string;
+//
+//   @IsOptional()
+//   @IsString()
+//   password?: string;
+//
+//   @IsOptional()
+//   @IsString()
+//   lastName?: string;
+//
+//   @IsOptional()
+//   @IsString()
+//   firstName?: string;
+//
+// @IsOptional()
+// @Transform(({ value }) => {
+//   if (!value) return undefined;
+//   // Обрабатываем как строку "dd/mm/yyyy" или Date
+//   if (value instanceof Date) return value;
+//   const [day, month, year] = value.split('/');
+//   return new Date(`${year}-${month}-${day}`);
+// })
+// @IsDate()
+// birthDate?: Date;
+// }
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,23 +1,38 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { plainToInstance } from 'class-transformer';
 import { UserResponseDto } from './dto/response-user.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prismaService: PrismaService) {
-  }
+  constructor(private readonly prismaService: PrismaService) {}
 
   async create(dto: CreateUserDto): Promise<User> {
     const { email, password, firstName, lastName, birthDate } = dto;
 
+    const hashedPassword = await argon2.hash(password, {
+      type: argon2.argon2id, // Используем argon2id для лучшей безопасности
+      memoryCost: 2 ** 16, // 64 MB
+      timeCost: 3, // Количество итераций
+      parallelism: 1, // Параллельность
+    });
+
+    // Проверяем, существует ли уже пользователь с таким email
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException(`Пользователь с email ${email} уже существует`);
+    }
+
     const user = await this.prismaService.user.create({
       data: {
         email,
-        password,
+        password: hashedPassword,
         firstName,
         lastName,
         birthDate, // Ensure the date is stored correctly
@@ -49,7 +64,7 @@ export class UserService {
       select: {
         id: true,
         email: true,
-        // password: true,
+        password: true,
         firstName: true,
         lastName: true,
         birthDate: true,
@@ -65,18 +80,32 @@ export class UserService {
   async update(id: string, dto: UpdateUserDto): Promise<User> {
     const user = await this.findOne(id);
 
-    const userUpdate = await this.prismaService.user.update({
+    const password = dto.password
+      ? await argon2.hash(dto.password, {
+          type: argon2.argon2id,
+          memoryCost: 2 ** 16, // 64 MB
+          timeCost: 3, // Количество итераций
+          parallelism: 1, // Параллельность
+        })
+      : user.password;
+
+    const currentBirthDate =
+      typeof user.birthDate === 'string'
+        ? new Date(user.birthDate.split('/').reverse().join('-'))
+        : user.birthDate;
+
+    const updatedUser = await this.prismaService.user.update({
       where: { id: user.id },
       data: {
-        email: dto.email || user.email,
-        password: dto.password || user.password,
-        firstName: dto.firstName || user.firstName,
-        lastName: dto.lastName || user.lastName,
-        birthDate: dto.birthDate || user.birthDate, // Ensure the date is stored correctly
+        email: dto.email ?? user.email,
+        password,
+        firstName: dto.firstName ?? user.firstName,
+        lastName: dto.lastName ?? user.lastName,
+        birthDate: dto.birthDate ?? currentBirthDate,
       },
     });
 
-    return userUpdate;
+    return updatedUser;
   }
 
   async remove(id: string) {
